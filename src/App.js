0031d39ff8ac19be50396883509904b932c74d94
Bryant-Wang24 <485969746@qq.com>
2021-01-19 23:28:05 +0800

    第一次提交

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super()
+    this.state = { x: 1, y: 2 }
+  }
+  onClick = () => {
+    this.setState({
+      x: this.state.x + 1,
+    })
+    this.setState({
+      x: this.state.x + 1,
+    })
+  }
+  // onClick = () => {
+  //   this.setState({
+  //     x: this.state.x + 1,
+  //   }, () => {
+  //     this.setState({
+  //       x: this.state.x + 1,
+  //     })
+  //   })
+  // }
+  onClick2 = () => {
+    this.setState((state) => ({ x: state.x + 2 }))
+    this.setState((state) => ({ x: state.x + 2 }))
+  }
+  render() {
+    return (
+      <div className="App">
+        App<button onClick={this.onClick}>+1</button>
+        <B name={this.state.x} />
+      </div>
+    )
+  }
 }
+
+class B extends React.Component {
+  componentWillReceiveProps(nextProps, nextContent) {
+    console.log('props变化了');
+  }
+  render() {
+    return (
+      <div>{this.props.name}</div>
+    )
+  }
+}
+
 
 export default App;
